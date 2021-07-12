56e88528db081ab5118d09e6efb6a5ea0a202317
Marine Incera <marine.incera@gmail.com>
2021-07-12 22:53:51 +0200

    cityPage: get id with the router

--- src/app/Pages/city-page/city-page.component.ts
+++ src/app/Pages/city-page/city-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-city-page',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CityPageComponent implements OnInit {
 
-  constructor() { }
+  constructor( private route: ActivatedRoute, private router: Router,) { }
 
   ngOnInit(): void {
+    // Get city id 
+    const id = this.route.snapshot.paramMap.get("id");
   }
 
 }
