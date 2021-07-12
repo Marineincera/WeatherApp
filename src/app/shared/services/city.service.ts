8c52fa023e9f56b27e7ceba9418bb5345ccf3b98
Marine Incera <marine.incera@gmail.com>
2021-07-12 18:16:32 +0200

    service: api link added to city service

--- src/app/shared/services/city.service.ts
+++ src/app/shared/services/city.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
+  static URL = "https://secret-ocean-49799.herokuapp.com/https://www.metaweather.com/api/location/search/?query=";
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getCity(name: string){
+    return this.http.get(CityService.URL + name)
+  }
 }
