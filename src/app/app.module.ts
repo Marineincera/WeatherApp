fcb492a26bcceef4c29b9f082dda56bb866215b3
Marine Incera <marine.incera@gmail.com>
2021-07-12 17:20:11 +0200

    build: cityPage component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './Pages/homepage/homepage.component';
+import { CityPageComponent } from './Pages/city-page/city-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    CityPageComponent
   ],
   imports: [
     BrowserModule,
