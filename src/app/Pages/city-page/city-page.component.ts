dabf70d6b1b0b1292fa0990bb1866dfeac0cdd0b
Marine Incera <marine.incera@gmail.com>
2021-07-14 17:29:25 +0200

    cityPage: get city infos thanks to the id

--- src/app/Pages/city-page/city-page.component.ts
+++ src/app/Pages/city-page/city-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { City } from 'src/app/shared/models/city';
+import { CityService } from 'src/app/shared/services/city.service';
 
 @Component({
   selector: 'app-city-page',
@@ -7,12 +9,39 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./city-page.component.scss']
 })
 export class CityPageComponent implements OnInit {
-
-  constructor( private route: ActivatedRoute, private router: Router,) { }
+  cityId : number | null = 0;
+  city : City = {
+    title: "City",
+    location_type: "",
+    woeid: 0,
+    distance: 0
+  }
+  constructor( private route: ActivatedRoute, private router: Router,private cityService: CityService) { }
 
   ngOnInit(): void {
     // Get city id 
-    const id = this.route.snapshot.paramMap.get("id");
+    this.cityId = Number(this.route.snapshot.paramMap.get("id"));
+    //Get city infos 
+    if(this.cityService.initializationCities.length > 0){
+      //From city service
+      this.city = this.getInfosFromService(this.cityId)
+      console.log(this.city)
+    } else {
+      //From API
+      this.getCityInfoFromApi(this.cityId)
+    }
+
+
+  }
+
+ getInfosFromService(id: number){
+     return this.cityService.initializationCities.find((city: City) => city.woeid === id);
+  }
+  
+  getCityInfoFromApi(id: number) : any{
+    this.cityService.getCityById(id).subscribe((cityArray: any) => {
+      return this.city = cityArray
+    })
   }
 
 }
--- src/app/shared/services/city.service.ts
+++ src/app/shared/services/city.service.ts
@@ -6,12 +6,16 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CityService {
-  static URL = "https://secret-ocean-49799.herokuapp.com/https://www.metaweather.com/api/location/search/?query=";
+  static URL = "https://secret-ocean-49799.herokuapp.com/https://www.metaweather.com/api/location/";
   initializationCities : Array<any>=[]
 
   constructor(private http: HttpClient) { }
 
-  getCity(name: string){
-    return this.http.get(CityService.URL + name)
+  getCity(name: string | null){
+    return this.http.get(CityService.URL + "search/?query=" + name)
+  }
+
+  getCityById(id: number){
+    return this.http.get(CityService.URL + "/api/location/" + id)
   }
 }
